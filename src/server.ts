c357322cdabe8e4c5c8dbb6f888248ad489f24ef
Ndima-karabo17 <203499810+Ndima-karabo17@users.noreply.github.com>
2026-03-02 09:06:48 +0200

    Update typescript HTTP server and env configuration

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import http from 'http';
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ message: 'Shopping List API is running' }));
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = Number(process.env.PORT) || 3000;
 
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
